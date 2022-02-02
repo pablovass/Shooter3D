1433ad09823ff635bc5b3224f67d9e67ac446f7f
pablovass <faktory.fm@live.com>
2022-02-02 13:33:17 -0300

    enemy point done

---
+++ Assets/_Scripts/EnemyManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Enemy enemy;
+    public float spawnTime = 3f;
+    void Start(){
+     InvokeRepeating("Spawn", spawnTime,spawnTime);   
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemy, this.transform.position, this.transform.rotation);
+    }
+    
+}
